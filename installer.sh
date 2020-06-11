34684f91864192b20639579a11b2762d656f2608
Cory Worthen <cworthen@visionarybroadband.com>
2020-06-11 17:28:14 -0600

    updated symlink for superuser

--- installer.sh
+++ installer.sh
@@ -323,6 +323,12 @@ if [ "$EUID" -ne 0 ]
                 echo "Failed to create vlooper symlink, this is necessary in order for the omxlooper service to run on boot!"
                 echo "  - Please manually create this with: ln -s ~/vlooper/vlooper.sh /usr/local/bin/vlooper"
         fi
+        echo "Creating superuser symbolic link to vloop_boot script..."
+        if ! echo "$sudoPW" | sudo -S -k ln -s "$HOME/vlooper/vlooper_boot.sh" /usr/local/bsin/vlooper_boot
+            then
+                echo "Failed to create vlooper superuser symlink, this is necessary in order for the omxlooper service to run on boot!"
+                echo "  - Please manually create this with: ln -s ~/vlooper/vlooper.sh /usr/local/bin/vlooper"
+        fi
     else
         echo "Creating symbolic link to vupdate script..."
         if ! ln -s ~/vlooper/vupdate.sh /usr/local/sbin/vupdate
