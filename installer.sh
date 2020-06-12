58086c05ea060867fe147b314470999552f0bf80
Cory Worthen <cworthen@visionarybroadband.com>
2020-06-11 19:42:55 -0600

    fixed source path being commented out still

--- installer.sh
+++ installer.sh
@@ -129,17 +129,17 @@ if ! sed -i'' -e "s,create 660,create 660 $USER $USER,g" ./examples/vlogrotate.e
 fi
 
 # Update both scripts to specify the full filepath to the main.cfg
-if ! sed -i'' -e "s,source,source $HOME/vlooper/inc/main.cfg,g" ./examples/vlooper.example
+if ! sed -i'' -e "s,#source,source $HOME/vlooper/inc/main.cfg,g" ./examples/vlooper.example
     then
         echo "Failed to update vlooper script with main.cfg path"
         exit 1
 fi
-if ! sed -i'' -e "s,source,source $HOME/vlooper/inc/main.cfg,g" ./examples/vlooper_boot.example
+if ! sed -i'' -e "s,#source,source $HOME/vlooper/inc/main.cfg,g" ./examples/vlooper_boot.example
     then
         echo "Failed to update vlooper_boot script with main.cfg path"
         exit 1
 fi
-if ! sed -i'' -e "s,source,source $HOME/vlooper/inc/main.cfg,g" ./examples/vupdate.example
+if ! sed -i'' -e "s,#source,source $HOME/vlooper/inc/main.cfg,g" ./examples/vupdate.example
     then
         echo "Failed to update vupdate script with main.cfg path"
         exit 1
